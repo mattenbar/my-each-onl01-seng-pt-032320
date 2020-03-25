43dbe5163ab131686500649cc6604d5d6b7cc83d
matthew enbar <matthew.enbar@gmail.com>
2020-03-25 23:51:08 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,6 @@
-def my_each(array)
-  i=0
-  while i < array.length
-      yield array[i]
-      i = i + 1
-  end
-  array
+def my_each(array) # put argument(s) here
+  i= 0
+  while i < array.size
+    i = i + 1
   end
 end
